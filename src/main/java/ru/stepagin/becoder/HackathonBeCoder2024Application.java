abb40e83b0913e511168813b1168bf45e9811113
Stepan <77981177+Maskvizy@users.noreply.github.com>
2024-04-07 15:18:11 +0300

    init commit

---
+++ src/main/java/ru/stepagin/becoder/HackathonBeCoder2024Application.java
@@ -0,0 +1,13 @@
+package ru.stepagin.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBeCoder2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonBeCoder2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/stepagin/becoder/HackathonBeCoder2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepagin.becoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBeCoder2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
