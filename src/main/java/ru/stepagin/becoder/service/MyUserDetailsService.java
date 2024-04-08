b5aac1ca1ea2893a175f769cd450266a29185535
themyav <Myavochka119@gmail.com>
2024-04-08 14:09:07 +0300

    added UserDetails and SecurityConfiguration

---
+++ src/main/java/ru/stepagin/becoder/service/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package ru.stepagin.becoder.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.stepagin.becoder.entity.PersonEntity;
+import ru.stepagin.becoder.repository.PersonRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private final PersonRepository personRepository;
+
+    public MyUserDetailsService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)  throws UsernameNotFoundException {
+        PersonEntity user = personRepository.findByLogin(username); //username = email
+        if(user == null) throw new UsernameNotFoundException("нет такого пользователя");
+        return User.builder()
+                .username(user.getLogin())
+                .password(user.getPassword())
+                .build();
+    }
+}
