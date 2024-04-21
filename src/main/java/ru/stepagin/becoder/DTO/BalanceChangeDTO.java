f30f23772e313cbe94f9f0135f2e05d4dd0f7411
Stepan <77981177+Maskvizy@users.noreply.github.com>
2024-04-22 00:27:23 +0300

    update BalanceChangeDTO: создано 2 конструктора: для обработки целых чисел (рублей) и дробных чисел (рублей с копейками)

--- src/main/java/ru/stepagin/becoder/DTO/BalanceChangeDTO.java
+++ src/main/java/ru/stepagin/becoder/DTO/BalanceChangeDTO.java
@@ -13,4 +13,28 @@ public class BalanceChangeDTO {
     private Long amount;
     @Nonnull
     private LegalAccountDTO account;
+
+    /**
+     * Конструктор для корректной обработки копеек. Используются минорные единицы - копейки.
+     *
+     * @param amount  double значение изменения баланса. Конвертируется в long значение копеек.
+     * @param account данные юридического счёта.
+     */
+    public BalanceChangeDTO(double amount, @Nonnull LegalAccountDTO account) {
+        long rubles = (long) amount;
+        long kopecks = (long) ((amount - rubles) * 100);
+        this.amount = rubles * 100 + kopecks;
+        this.account = account;
+    }
+
+    /**
+     * Конструктор с обработкой целых рублей.
+     *
+     * @param rubles  число рублей.
+     * @param account данные юридического счёта.
+     */
+    public BalanceChangeDTO(long rubles, @Nonnull LegalAccountDTO account) {
+        this.amount = rubles * 100;
+        this.account = account;
+    }
 }
