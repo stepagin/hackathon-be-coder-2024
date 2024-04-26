f5ff3e8e8e5271f5e7f40fbedd3fd842107291ae
Stepan <77981177+Maskvizy@users.noreply.github.com>
2024-04-26 12:13:18 +0300

    change BalanceChangeDto: removed unnecessary comment

--- src/main/java/ru/stepagin/becoder/dto/BalanceChangeDto.java
+++ src/main/java/ru/stepagin/becoder/dto/BalanceChangeDto.java
@@ -14,18 +14,6 @@ public class BalanceChangeDto {
     @Positive(message = "должен быть больше нуля")
     private double amount;
 
-//    /**
-//     * Сеттер для автоматического создания BalanceChangeDto в контроллерах.
-//     *
-//     * @param amount сумма изменения дробным числом в рублях.
-//     *               Цифры после второго знака после запятой будут проигнорированы.
-//     */
-//    public void setAmount(Double amount) {
-//        long rubles = (long) (double) amount;
-//        long kopecks = Math.round((amount - rubles) * 1000) / 10;
-//        this.amount = rubles * 100 + kopecks;
-//    }
-
     public long getAmountInKopecks() {
         long rubles = (long) amount;
         long kopecks = Math.round((amount - rubles) * 1000) / 10;
